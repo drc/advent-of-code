5d7bac86ebf5c11d26ec477b535def9de31c3862
Dan Cigrang <danielcigrang@me.com>
2021-12-01 13:29:52 -0600

    two star day 1

--- 2021/day01/index.js
+++ 2021/day01/index.js
@@ -2,7 +2,7 @@ import { readInput, readSampleInput } from "../../utils/readInput.mjs";
 import assert from "assert";
 
 const rawSample = readSampleInput();
-const sample = rawSample.split("\n");
+const sample = rawSample.split("\n").map(val => parseInt(val));
 
 const rawData = readInput();
 const raw = rawData.split("\n").map(val => parseInt(val));
@@ -20,8 +20,32 @@ const partOne = data => {
     ).count;
 };
 
-const answer = partOne(raw);
+const answer = partOne(sample);
+assert.strictEqual(answer, 7);
 
-// assert.strictEqual(answer, 7);
+const finalAnswer = partOne(raw);
 
-console.log(`Part 1 answer: ${answer}`);
+console.log(`Part 1 answer: ${finalAnswer}`);
+
+const partTwo = data => {
+    return data.reduce(
+        (acc, val, index, arr) => {
+            if (!arr[index + 2]) return acc;
+            const roll = val + arr[index + 1] + arr[index + 2];
+            if (acc.prev !== null && acc.prev < roll) {
+                acc.count++;
+            }
+            acc.prev = roll;
+            return acc;
+        },
+        { prev: null, count: 0 }
+    ).count;
+};
+
+const answer2 = partTwo(sample);
+assert.strictEqual(answer2, 5);
+
+const finalAnswer2 = partTwo(raw);
+
+
+console.log(`Part 2 answer: ${finalAnswer2}`);
