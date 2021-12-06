a1e0a79f4326e4f3775defaf952f3d8779603ed2
Dan Cigrang <danielcigrang@me.com>
2021-12-06 11:15:00 -0600

    day 6 part 2 is not what i thought it was gonna be, had to pivot

--- 2021/day06/index.js
+++ 2021/day06/index.js
@@ -3,34 +3,35 @@ import assert from "assert";
 import LanternFish from "./LanternFish.js";
 
 const rawSample = readSampleInput();
-const sample = rawSample.split(",");
+const sample = rawSample.split(",").map(n => +n);
 
 const rawData = readInput();
-const raw = rawData.split(",");
+const raw = rawData.split(",").map(n => +n);
 
 const getStartingFish = data => {
     const pool = [];
-    for(let timer of data) {
-        pool.push(new LanternFish(+timer));
+    for (let timer of data) {
+        pool.push(new LanternFish(timer));
     }
     return pool;
-}
+};
 
 const newDay = fishList => {
     const newFish = [];
-    for(let fish of fishList) {
+    for (let fish of fishList) {
         fish.addDay();
-        if(fish.hasNewFish()) {
+        if (fish.hasNewFish()) {
             newFish.push(fish.createNewFish());
         }
     }
     return fishList.concat(newFish);
-}
+};
 
+// leaving part 1 for showing thought process
 const partOne = (data, days) => {
     let fishList = getStartingFish(data);
     var i = 0;
-    while(i < days) {
+    while (i < days) {
         fishList = newDay(fishList);
         i++;
     }
@@ -43,12 +44,22 @@ assert.strictEqual(answer, 5934);
 const finalAnswer = partOne(raw, 80);
 console.log(`Part 1 answer: ${finalAnswer}`);
 
+// didnt realize part two would have been this ;)
 const partTwo = (data, days) => {
-    return partOne(data, days);
+    const fish = Array(9).fill(0); // 0-8
+    // count number of fish timers
+    data.forEach(n => fish[n]++); 
+    for (let day = 0; day < days; day++) {
+        // add to 7 because we're removing first element which then makes it 6
+        fish[7] += fish[0]; 
+        // remove the count at timer 0 and add to the end (reset)
+        fish.push(fish.shift()); 
+    }
+    return fish.reduce((acc, val) => acc + val, 0);
 };
 
 const answer2 = partTwo(sample, 256);
 assert.strictEqual(answer2, 26984457539);
 
-// const finalAnswer2 = partTwo(raw);
-// console.log(`Part 2 answer: ${finalAnswer2}`);
+const finalAnswer2 = partTwo(raw, 256);
+console.log(`Part 2 answer: ${finalAnswer2}`);
