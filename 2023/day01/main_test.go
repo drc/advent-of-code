aac8816fed78478143859722f2c862ade3dbd508
Dan Cigrang <danielcigrang@me.com>
2023-11-29 20:18:30 -0600

    setup for 2023 using go

---
+++ 2023/day01/main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_part1(t *testing.T) {
+	t.Run("Test Part 1", func(t *testing.T) {
+		if got := part1(input); got != 0 {
+			t.Errorf("part1() = %v, want %v", got, 0)
+		}
+	})
+}
+
+func Test_part2(t *testing.T) {
+	t.Run("Test Part 2", func(t *testing.T) {
+		if got := part1(input); got != 0 {
+			t.Errorf("part1() = %v, want %v", got, 0)
+		}
+	})
+}
---
+++ 2023/go.mod
@@ -0,0 +1,3 @@
+module github.com/drc/advent-of-code
+
+go 1.21.4
