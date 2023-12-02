290e46c752a7808cfef3296a74f270dcd1ca8541
Dan Cigrang <danielcigrang@me.com>
2023-12-02 09:10:03 -0600

    day 2 part 1

--- 2023/day02/main.go
+++ 2023/day02/main.go
@@ -4,6 +4,8 @@ import (
 	_ "embed"
 	"flag"
 	"fmt"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -25,6 +27,7 @@ func main() {
 
 	if part == 1 {
 		ans := part1(input)
+		// > 2688
 		fmt.Println("Output:", ans)
 	} else {
 		ans := part2(input)
@@ -32,8 +35,80 @@ func main() {
 	}
 }
 
+var (
+	bag = Set{
+		red:   12,
+		green: 13,
+		blue:  14,
+	}
+)
+
+type Set struct {
+	red   int
+	green int
+	blue  int
+}
+
 func part1(input string) int {
-	return 0
+	var total int = 0
+	lines := strings.Split(input, "\n")
+
+	for _, line := range lines {
+		var isValid bool = true
+		gameID, sets := unpackSets(line)
+		games := convToSet(sets)
+
+		for _, game := range games {
+			if !game.valid() {
+				// invalid game
+				isValid = false
+				break
+			}
+		}
+		if isValid {
+			total += gameID
+		}
+	}
+	return total
+}
+
+func unpackSets(line string) (int, []string) {
+	slice := strings.Split(line, ":")
+	re := regexp.MustCompile(`Game (\d{1,3})`)
+	gameID, _ := strconv.Atoi(re.FindStringSubmatch(slice[0])[1])
+	return gameID, strings.Split(slice[1], ";")
+}
+
+func convToSet(sets []string) []Set {
+	var setlist []Set
+	re := regexp.MustCompile(`(\d{1,3})\s(red|green|blue)`)
+
+	for _, group := range sets {
+		var currentSet Set
+		pairs := re.FindAllStringSubmatch(group, -1)
+		for _, pair := range pairs {
+			cubes, color := unpackPair(pair)
+			switch color {
+			case "red":
+				currentSet.red = cubes
+			case "green":
+				currentSet.green = cubes
+			case "blue":
+				currentSet.blue = cubes
+			}
+		}
+		setlist = append(setlist, currentSet)
+	}
+	return setlist
+}
+
+func unpackPair(pair []string) (int, string) {
+	cubes, _ := strconv.Atoi(pair[1])
+	return cubes, pair[2]
+}
+
+func (s Set) valid() bool {
+	return s.red <= bag.red && s.green <= bag.green && s.blue <= bag.blue
 }
 
 func part2(input string) int {
--- 2023/day02/main_test.go
+++ 2023/day02/main_test.go
@@ -4,13 +4,17 @@ import (
 	"testing"
 )
 
-var example string = `hot
-damn`
+var example string = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`
 
 func Test_part1(t *testing.T) {
+	expected := 8
 	t.Run("Test Part 1", func(t *testing.T) {
-		if got := part1(example); got != 0 {
-			t.Errorf("part1() = %v, want %v", got, 0)
+		if got := part1(example); got != expected {
+			t.Errorf("part1() = %v, want %v", got, expected)
 		}
 	})
 }
