39dd667e8f17544c73d4481c22862c4d18721211
Dan Cigrang <danielcigrang@me.com>
2023-12-04 13:03:44 -0600

    day 3 where i left off

--- 2023/day03/main.go
+++ 2023/day03/main.go
@@ -4,6 +4,8 @@ import (
 	_ "embed"
 	"flag"
 	"fmt"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -33,7 +35,48 @@ func main() {
 }
 
 func part1(input string) int {
-	return 0
+	var total int = 0
+	lines := strings.Split(input, "\n")
+
+	reNumber := regexp.MustCompile(`\d+`)
+	reSymbol := regexp.MustCompile(`[^\d\.]`)
+
+	for lineIndex, line := range lines {
+		numberMatches := reNumber.FindAllStringSubmatch(line, -1)
+		numMatchIndex := reNumber.FindAllStringIndex(line, -1)
+		// fmt.Printf("line --> %#v\n\n", line)
+		for _, numMatch := range numberMatches {
+			fmt.Printf("%+v\n\n", numMatch)
+			// check left
+			if len(numMatchIndex) > 0 && numMatchIndex[0][0] != 0 {
+				symbolMatch := reSymbol.FindAllStringSubmatch(string(line[numMatchIndex[0][0]-1]), -1)
+				if len(symbolMatch) > 0 {
+					validNumber, _ := strconv.Atoi(numMatch[0])
+					total += validNumber
+				}
+			}
+			// check right
+			if numMatchIndex[0][1] < len(line) {
+				fmt.Printf("substring --> %#v\n\n", string(line[numMatchIndex[0][1]]))
+				symbolMatch := reSymbol.FindAllStringSubmatch(string(line[numMatchIndex[0][1]]), -1)
+				fmt.Printf("symbolMatch --> %#v\n\n", symbolMatch)
+				if len(symbolMatch) > 0 {
+					validNumber, _ := strconv.Atoi(numMatch[0])
+					total += validNumber
+				}
+			}
+			// check top + diagonal
+			// first row doesnt have a line above
+			if lineIndex != 0 {
+				lineAbove := lines[lineIndex-1]
+				fmt.Printf("line above --> %#v\n", lineAbove)
+			}
+			// check bottom + diagonal
+		}
+
+	}
+
+	return total
 }
 
 func part2(input string) int {
--- 2023/day03/main_test.go
+++ 2023/day03/main_test.go
@@ -4,13 +4,22 @@ import (
 	"testing"
 )
 
-var example string = `hot
-damn`
+var example string = `467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..`
 
 func Test_part1(t *testing.T) {
+	expected := 4361
 	t.Run("Test Part 1", func(t *testing.T) {
-		if got := part1(example); got != 0 {
-			t.Errorf("part1() = %v, want %v", got, 0)
+		if got := part1(example); got != expected {
+			t.Errorf("part1() = %v, want %v", got, expected)
 		}
 	})
 }
