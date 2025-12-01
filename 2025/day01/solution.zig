d97380a852a6091cfa972a2e605bb0c662ca757a
Dan Cigrang <danielcigrang@me.com>
2025-12-01 16:22:42 -0600

    working against the example

--- 2025/day01/solution.zig
+++ 2025/day01/solution.zig
@@ -19,17 +19,44 @@ const example =
     \\L82
 ;
 
-test "aoc example" {
-    try task1(example);
+const expect = std.testing.expect;
+
+test "aoc example task 1" {
+    const result = try task1(example);
+    print("{}\n\n", .{result});
+    try expect(result == 3);
 }
 
-fn task1(input: []const u8) !void {
-    const starting_position = 50;
+test "aoc example task 2" {
+    try task2();
+}
+
+const Direction = enum(u8) { left = 'L', right = 'R' };
+
+fn task1(input: []const u8) !i8 {
+    var position: i16 = 50;
+    var zeroes: i8 = 0;
     var lines = std.mem.tokenizeScalar(u8, input, '\n');
     while (lines.next()) |line| {
-        const direction = line[0];
-        const amount = try std.fmt.parseInt(u8, line[1..], 10);
+        const direction: Direction = @enumFromInt(line[0]);
+        const amount = try std.fmt.parseInt(i16, line[1..], 10);
 
-        print("\n[{d}] == {c} - {d}\n", .{ starting_position, direction, amount });
+        print("\n[{d}] == {t} - {d}\n", .{ position, direction, amount });
+
+        switch (direction) {
+            .left => {
+                // const new_pos: i16 = @intCast(@abs(position - amount));
+                position = @mod(position - amount, 100);
+            },
+            .right => {
+                position = @mod(position + amount, 100);
+            },
+        }
+        if (position == 0) {
+            zeroes += 1;
+        }
     }
+    return @intCast(zeroes);
 }
+
+fn task2() !void {}
